f0e2a0fb3b6d0f9e8a6ce3ccb4049a595fc45c03
ashugithub94 <146065157+ashugithub94@users.noreply.github.com>
2025-01-04 08:24:35 +0000

    loops practice

---
+++ today.js
@@ -0,0 +1,46 @@
+for (let index = 0; index <=10 ; index++) {
+
+    for (let j = 0; j <= 10; j++) {
+     //  console.log(index +'*'+ j +'='+ index*j)
+        
+    }
+  
+    
+}
+
+const arr =[1,2,3,4]
+
+//const b = arr.push(7);  /* push method changes in real aaray push a element at last*\
+
+//const b = arr.shift();  /*  shit doesnot take any args its just remove value of 1 index*\
+//const b = arr.unshift(0); /*  unshit  take  args its and add at value of 1 index*\
+ 
+ //b = arr.slice(1,3)/*  slice   take  args and add it at strat and end point the value of b is 2,3  and real aaray remains same*\
+ b = arr.splice(1,3)
+//console.log(`the value of b is ${b} and arr is${arr}`);// the value of b is 2,3,4 and arr is1
+
+let i =1
+
+// while (i<=10) {
+//  //   console.log(i);
+// //    i++
+    
+// }
+
+// do {
+    
+//     console.log(i);
+//     i++ 
+// } while (i<=10);
+
+
+const str = 'ashu pand'
+
+for (const boobs of str) {
+    console.log(boobs);
+    
+}
+
+
+
+
